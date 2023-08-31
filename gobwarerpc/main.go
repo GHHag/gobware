963dd58374c351c4d7cfb5fb4056e05079d6bde8
GHHag <gustav.hagenblad@gmail.com>
2023-08-31 22:55:47 +0200

    pb defs

--- gobwarerpc/main.go
+++ gobwarerpc/main.go
@@ -1,17 +1,55 @@
 package main
 
-import(
-	"fmt"
-	// "context"
+import (
+	"context"
 	// "log"
-	// "google.golang.org/grpc"
-	// pb "gobwarerpc"
+	"net"
+	"google.golang.org/grpc"
+	pb "gobwarerpc/gobwarerpc"
 )
 
 type server struct {
 	pb.UnimplementedGobwareServiceServer
 }
 
-func main(){
-	fmt.Println("Hello gobware")
+func(s *server) AddACLRule(ctx context.Context, req *pb.AddACLRuleRequest) (*AddACLRuleResponse, error) {
+
+}
+
+func(s *server) SetACL(ctx context.Context, ) (, error) {
+
+}
+
+func(s *server) CreateToken(ctx context.Context, ) (*CreateTokenResponse, error) {
+
+}
+
+func(s *server) CreateTokenPair(ctx context.Context, ) (*CreateTokenPairResponse, error) {
+
+}
+
+func(s *server) CheckAccess(ctx context.Context, ) (, error) {
+
+}
+
+func(s *server) CheckToken(ctx context.Context, ) (, error) {
+
+}
+
+func(s *server) Adapt(ctx context.Context, ) (, error) {
+
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGobwareServiceServer(s, &server{})
+
+	if err := s.Serve(listener); err != nil {
+		panic(err)
+	}
 }
