e32a46eff6eea22d1d6cf426f9ab1b6fd93dba8c
GHHag <gustav.hagenblad@gmail.com>
2023-09-01 23:23:23 +0200

    pb defs

--- gobwarerpc/main.go
+++ gobwarerpc/main.go
@@ -12,32 +12,47 @@ type server struct {
 	pb.UnimplementedGobwareServiceServer
 }
 
-func(s *server) AddACLRule(ctx context.Context, req *pb.AddACLRuleRequest) (*AddACLRuleResponse, error) {
+func(s *server) AddACLRule(ctx context.Context, req *pb.AddACLRuleRequest) (*pb.AddACLRuleResponse, error) {
+	ACLRule := make(map[string]map[string]bool)
 
+	res := &pb.AddACLRuleResponse {
+		ACLRule: ACLRule,
+	}
+
+	return res, nil
 }
 
-func(s *server) SetACL(ctx context.Context, ) (, error) {
-
+func(s *server) SetACL(ctx context.Context, req *pb.SetACLRequest) (error) {
+	return nil
 }
 
-func(s *server) CreateToken(ctx context.Context, ) (*CreateTokenResponse, error) {
+func(s *server) CreateToken(ctx context.Context, req *pb.CreateTokenRequest) (*pb.CreateTokenResponse, error) {
+	res := &pb.CreateTokenResponse {
+		encodedToken: "test",
+	}
 
+	return res, nil
 }
 
-func(s *server) CreateTokenPair(ctx context.Context, ) (*CreateTokenPairResponse, error) {
+func(s *server) CreateTokenPair(ctx context.Context, req *pb.CreateTokenPairRequest) (*pb.CreateTokenPairResponse, error) {
+	res := &pb.CreateTokenPairResponse {
+		encodedAccessToken: "test",
+		encodedRefreshToken: "test",
+	}
 
+	return res, nil
 }
 
-func(s *server) CheckAccess(ctx context.Context, ) (, error) {
-
+func(s *server) CheckAccess(ctx context.Context) (error) {
+	return nil
 }
 
-func(s *server) CheckToken(ctx context.Context, ) (, error) {
-
+func(s *server) CheckToken(ctx context.Context) (error) {
+	return nil
 }
 
-func(s *server) Adapt(ctx context.Context, ) (, error) {
-
+func(s *server) Adapt(ctx context.Context) (error) {
+	return nil
 }
 
 func main() {
